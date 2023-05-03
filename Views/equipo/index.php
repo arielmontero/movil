1c15c53269b7071fbf9409e8ff5ec09a8ded8c13
arielmonteroarg <arielmontero.arg@gmail.com>
2023-05-03 11:59:31 -0300

    cambio en los index para agregar el datatables

--- Views/equipo/index.php
+++ Views/equipo/index.php
@@ -14,21 +14,26 @@ $res=$equipo->ControlPermiso($us,$per);
 if ($res== 1) {
 	?>
 <div class="box-principal">
-	<h3 class="titulo">Equipo</h3>
 		<div class="panel panel-success">
-			<div class="panel-heading">
-				<table class="table table-striped table-hover">
-					<td><h3 class="panel-title"><a href="<?php echo URL;?>equipo/crear" class="btn btn-success">Crear Nuevo</a></h3></td>
-					<td>
-						<form class="form-inline my-2 my-lg-0" action="<?php echo URL;?>equipo/buscar" method="POST">
-				      <input class="form-control mr-sm-2" type="text" name="Equipo" placeholder="Equipo">
-				      <button class="btn btn-secondary my-2 my-sm-0"  type="submit">Buscar</button>
-				    </form>
-					</td>
-			</table>
-			</div>
+			<ol class="breadcrumb">
+			<li class="breadcrumb-item"><a href="<?php echo URL;?>"><i class="fas fa-home" aria-hidden="true"></i> Home</a></li>
+			<li class="breadcrumb-item"><a href="<?php echo URL;?>equipo">Equipo</a></li>
+			<li class="breadcrumb-item active" aria-current="page">index</li>
+		</ol>
+		<div class="card">  
+		<div class="card-header" style="background-color: rgba(68, 138, 255);">
+			<h4 class="card-title" style="color: rgba(255,255,255,0.9);"><b>Equipo</b></h4>    
+		</div>
+
+		<nav aria-label="breadcrumb">  
+		<div class="card">  
+		<div class="card-body">
+		<h3 class="panel-title"><a href="<?php echo URL;?>equipo/crear" class="btn btn-success">Crear Nuevo</a></h3>
+		</div>
+		</div>
+		</nav>  
 			<div class="panel-body">
-				<table class="table table-striped table-hover">
+				<table id="id_table" class="table table-striped table-hover">
 					<thead>
 						<th>Marca</th>
 						<th>Tipo</th>	
@@ -36,7 +41,7 @@ if ($res== 1) {
 						<th>Imei</th>	
 						<th>Estado</th>	
 						<th>Fecha</th>	
-						<th colspan="3">Action</th>
+						<th>Action</th>
 					</thead>
 					<tbody>
 					<?php while ($row = mysqli_fetch_array($datos)){ ?>
@@ -50,7 +55,7 @@ if ($res== 1) {
 						<td>
 						<a href="<?php echo URL;?>equipo/ver/<?php echo $row['ID_Equipo'];?>" class="btn btn-primary">Ver</a>
 						<a href="<?php echo URL;?>equipo/editar/<?php echo $row['ID_Equipo'];?>" class="btn btn-primary">Editar</a>
-						<a href="<?php echo URL;?>equipo/eliminar/<?php echo $row['ID_Equipo'];?>" class="btn btn-primary">Eliminar</a>
+						<a href="<?php echo URL;?>equipo/eliminar/<?php echo $row['ID_Equipo'];?>" class="btn btn-danger">Eliminar</a>
 						</td>
 						</tr>
 					<?php }	?>
